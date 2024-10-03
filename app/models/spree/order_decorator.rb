3ff39cdd3322a174123d999ff31b044d34806c74
Nikodem Gorak <nikos83@gmail.com>
2024-10-03 22:09:12 +0200

    added base indlude to override number generator

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,11 +1,9 @@
 module Spree
   module OrderDecorator
     def self.prepended(base)
-      # Usuwamy oryginalny mixin `NumberGenerator` z prefiksem 'R'
-      base.send(:include, Spree::Core::NumberGenerator.new(prefix: 'RORSEN'))
+      base.include Spree::Core::NumberGenerator.new(prefix: 'RORSEN')
     end
   end
 end
 
-# Przepinamy dekorator do klasy `Spree::Order`
 Spree::Order.prepend Spree::OrderDecorator
